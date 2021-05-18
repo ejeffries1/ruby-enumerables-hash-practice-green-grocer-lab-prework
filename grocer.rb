f1aea3798cc0ee7e42bd8ac5ca1ede04318be09e
Evan Jeffries <ejeffries14@gmail.com>
2021-05-18 19:53:18 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,25 @@
 def consolidate_cart(cart)
   # code here
+  cart_hash = {}
+  cart.each do |grocery|
+    grocery.each do |item, data|
+      if cart_hash[item]
+        cart_hash[item][:count] += 1
+      else
+        cart_hash[item] = data
+        cart_hash[item][:count] = 1
+      end
+    end
+  end
+  cart_hash
 end
 
 def apply_coupons(cart, coupons)
   # code here
+  coupons.each do |couponHash|
+    apply_single_coupon(cart, couponHash)
+  end
+  cart
 end
 
 def apply_clearance(cart)
